4d518287b44244207526feae956459f959175446
dscode89 <dscode89@gmail.com>
2024-09-11 18:42:19 +0100

    add listen.js file

---
+++ listen.ts
@@ -0,0 +1,11 @@
+import app from "./src/app";
+import createDatabaseConnection from "./src/database/createDatabaseConnection";
+
+createDatabaseConnection()
+  .then((mongoDb) => {
+    const { db } = mongoDb;
+    app.set("mongoDb", db);
+  })
+  .then(() => {
+    app.listen(9090);
+  });
