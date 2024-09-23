f7ccad8162676897b6e7ea91590f511d4e23558a
Daniel Saleh <dscode89@gmail.com>
2024-09-23 14:52:58 +0100

    update connection file to use production environment

--- dist/database/createDatabaseConnection.js
+++ dist/database/createDatabaseConnection.js
@@ -12,16 +12,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = createDatabaseConnection;
 const mongodb_1 = require("mongodb");
 const dotenv_1 = require("dotenv");
+const schemaValidationIndex_1 = require("./schemaValidationIndex");
 (0, dotenv_1.config)();
 function createDatabaseConnection() {
     return __awaiter(this, void 0, void 0, function* () {
-        const dbConnectionString = process.env.NODE_ENV
-            ? process.env.CONNECTION_STRING_TEST
-            : process.env.CONNECTION_STRING;
+        const dbConnectionString = process.env.NODE_ENV === "production"
+            ? process.env.CONNECTION_STRING
+            : process.env.NODE_ENV
+                ? process.env.CONNECTION_STRING_TEST
+                : process.env.CONNECTION_STRING;
         const client = yield mongodb_1.MongoClient.connect(dbConnectionString);
-        const dbName = process.env.NODE_ENV
-            ? process.env.DATABASE_NAME_TEST
-            : process.env.DATABASE_NAME;
+        const dbName = process.env.NODE_ENV === "production"
+            ? process.env.DATABASE_NAME // if it is - set it to dev
+            : process.env.NODE_ENV // if not - it the env variable set?
+                ? process.env.DATABASE_NAME_TEST // if so it must be test
+                : process.env.DATABASE_NAME; // if not - it's dev database
         const db = client.db(dbName);
         //if are connecting to the dev database
         if (dbName === process.env.DATABASE_NAME) {
@@ -31,88 +36,14 @@ function createDatabaseConnection() {
             if (!usersCollection) {
                 yield db.createCollection("Users", {
                     validator: {
-                        $jsonSchema: {
-                            title: "User Document Validation",
-                            required: [
-                                "_id",
-                                "firstName",
-                                "lastName",
-                                "preferredName",
-                                "role",
-                                "userPassword",
-                                "email",
-                            ],
-                            additionalProperties: false,
-                            properties: {
-                                _id: {
-                                    bsonType: "objectId",
-                                    description: "This is the generated ObjectId instance for a new inserted user document",
-                                },
-                                firstName: {
-                                    bsonType: "string",
-                                    description: "'firstName' must be a string and is required",
-                                },
-                                lastName: {
-                                    bsonType: "string",
-                                    description: "'lastName' must be a string and is required",
-                                },
-                                preferredName: {
-                                    bsonType: "string",
-                                    description: "'preferredName' must be a string and is required",
-                                },
-                                role: {
-                                    bsonType: "string",
-                                    description: "'firstName' must be a string of ADMIN or USER",
-                                },
-                                userPassword: {
-                                    bsonType: "string",
-                                    description: "'userPassword' must be a string and is required",
-                                },
-                                email: {
-                                    bsonType: "string",
-                                    description: "'email' must be a string and is required",
-                                },
-                            },
-                        },
+                        userValidationSchema: schemaValidationIndex_1.userValidationSchema,
                     },
                 });
             }
             if (!thoughtsCollection) {
                 yield db.createCollection("Thoughts", {
                     validator: {
-                        $jsonSchema: {
-                            title: "Thought Document Validation",
-                            required: [
-                                "_id",
-                                "userId",
-                                "thoughtMessage",
-                                "category",
-                                "isPriority",
-                            ],
-                            additionalProperties: false,
-                            properties: {
-                                _id: {
-                                    bsonType: "objectId",
-                                    description: "This is the generated ObjectId instance for a new inserted thought document",
-                                },
-                                userId: {
-                                    bsonType: "objectId",
-                                    description: "this is the objectId for the user who posted this thought",
-                                },
-                                thoughtMessage: {
-                                    bsonType: "string",
-                                    description: "thoughtMessage must be a string and is required",
-                                },
-                                category: {
-                                    bsonType: "string",
-                                    description: "'category' must be a string and of of the following values: BILLS | HOME | GENERAL",
-                                },
-                                isPriority: {
-                                    bsonType: "bool",
-                                    description: "'isPriority' must be a boolean and is required",
-                                },
-                            },
-                        },
+                        thoughtValidationSchema: schemaValidationIndex_1.thoughtValidationSchema,
                     },
                 });
             }
