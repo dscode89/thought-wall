35528fd4a0af1af0935d103508f77e66ccfda278
Daniel Saleh <dscode89@gmail.com>
2024-09-23 14:36:10 +0100

    delete unused files in dist folder

--- dist/database/dbIndex.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const createTestDb_1 = __importDefault(require("../database/createTestDb"));
-const createDevDb_1 = __importDefault(require("../database/createDevDb"));
-const currentDb = process.env.NODE_ENV ? createTestDb_1.default : createDevDb_1.default;
-exports.default = currentDb;
